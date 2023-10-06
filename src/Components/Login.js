5044f9237db1a555b9a239c558348cd0e6535f4f
ritikthapliyal <ritikthapliyal123@gmail.com>
2023-10-06 19:34:44 +0530

    fixed scanner

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import axios from "axios"
+import "./Login.css"
 
 export default function Login(){
 
@@ -11,7 +12,8 @@ export default function Login(){
     const handleLogin = async()=>{
 
         try{
-            const response=await axios.post("https://efce1xom36.execute-api.ap-south-1.amazonaws.com/dev/login",{username,password},{withCredentials:true})
+            const response=await axios.post("https://efce1xom36.execute-api.ap-south-1.amazonaws.com/dev/login",{username,password}, {withCredentials:true})
+            console.log(response.data.token)
             navigate('/Dashboard')
         }
         catch(err){
@@ -20,10 +22,12 @@ export default function Login(){
     }
 
     return(
+        <div className="barcode-scanner-login">
        <form  className="loginform" onSubmit={(e)=>{
         console.log(username,password)
         e.preventDefault()
        }}>
+        <div className="login-page-header">Login Page</div>
         <input onChange={(e)=>{
             setUsername(e.target.value)
         }} placeholder="username" value={username}  ></input>
@@ -32,6 +36,7 @@ export default function Login(){
         }} placeholder="password" value={password}></input>
         <button onClick={handleLogin}>Login</button>
        </form>
+       </div>
     )
 }
 
--- src/Components/Resultcontainerplugin.js
+++ src/Components/Resultcontainerplugin.js
@@ -1,57 +1,47 @@
-import React from 'react';
-
-function filterResults (results) {
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+function filterResults(results) {
     let filteredResults = [];
-    for (var i = 0; i < results.length; ++i) {
-        if (i === 0) {
-            filteredResults.push(results[i]);
-            continue;
-        }
-
-        if (results[i].decodedText !== results[i - 1].decodedText) {
-            filteredResults.push(results[i]);
-        }
-    }
     return filteredResults;
 }
-
 const ResultContainerTable = ({ data }) => {
     const results = filterResults(data);
+    console.log("data I am getting", results);
+    const navigate = useNavigate();
+    const postData = {
+        username: 'tanya',
+        barcode: results
+    };
+    useEffect(() => {
+        axios.post('https://efce1xom36.execute-api.ap-south-1.amazonaws.com/dev/postData', postData)
+            .then(response => {
+                console.log('POST Request Response:', response.data);
+                if (response.data.message) {
+                    navigate('/dashboard');
+                }
+            })
+            .catch(error => {
+                console.error('POST Request Error:', error);
+            });
+    }, [results, navigate]);
     return (
         <table className={'Qrcode-result-table'}>
-            <thead>
-                <tr>
-                    <td>#</td>
-                    <td>Decoded Text</td>
-                    <td>Format</td>
-                </tr>
-            </thead>
             <tbody>
                 {
                     results.map((result, i) => {
                         console.log(result);
-                        return (<tr key={i}>
-                            <td>{i}</td>
-                            <td>{result.decodedText}</td>
-                            <td>{result.result.format.formatName}</td>
-                        </tr>);
+                        return (
+                            <tr key={i}>
+                                <td>{i}</td>
+                                <td>{result.decodedText}</td>
+                                <td>{result.result.format.formatName}</td>
+                            </tr>
+                        );
                     })
                 }
             </tbody>
         </table>
     );
 };
-
-const ResultContainerPlugin = (props) => {
-    const results = filterResults(props.results);
-    return (
-        <div className='Result-container'>
-            <div className='Result-header'>Scanned results ({results.length})</div>
-            <div className='Result-section'>
-                <ResultContainerTable data={results} />
-            </div>
-        </div>
-    );
-};
-
-export default ResultContainerPlugin;
+export default ResultContainerTable;
