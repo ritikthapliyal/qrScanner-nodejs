f3e9c416b607e1ac184afb94c567a68e4992d44a
ritikthapliyal <ritikthapliyal123@gmail.com>
2023-10-07 14:24:34 +0530

    login finallll

--- src/Components/Resultcontainerplugin.js
+++ src/Components/Resultcontainerplugin.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 function filterResults(results) {
     let filteredResults = [];
@@ -9,22 +8,6 @@ const ResultContainerTable = ({ data }) => {
     const results = filterResults(data);
     console.log("data I am getting", results);
     const navigate = useNavigate();
-    const postData = {
-        username: 'tanya',
-        barcode: results
-    };
-    useEffect(() => {
-        axios.post('https://efce1xom36.execute-api.ap-south-1.amazonaws.com/dev/postData', postData)
-            .then(response => {
-                console.log('POST Request Response:', response.data);
-                if (response.data.message) {
-                    navigate('/dashboard');
-                }
-            })
-            .catch(error => {
-                console.error('POST Request Error:', error);
-            });
-    }, [results, navigate]);
     return (
         <table className={'Qrcode-result-table'}>
             <tbody>
