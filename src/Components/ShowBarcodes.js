55e0120ed842fb285aaab87d6e954800dd494660
ritikthapliyal <ritikthapliyal123@gmail.com>
2023-10-09 12:49:47 +0530

    testing ---3

--- src/Components/ShowBarcodes.js
+++ src/Components/ShowBarcodes.js
@@ -1,6 +1,7 @@
 import { useGetBarcodeQuery } from '../store/apis/dashboardApis'
 import Loading from './Loading'
 
+
 function ShowBarcodes() {
     
     const { data, isLoading } = useGetBarcodeQuery()
@@ -12,9 +13,18 @@ function ShowBarcodes() {
             }
             {
                 data && data.message && <div><h3>History :</h3>{data.message.map((obj,index)=>{
+
+
+                    //date time calculate
+                    const dateObject = new Date(date)
+
+                    const formattedTime = dateObject.toLocaleTimeString()
+                    const formattedDate = dateObject.toLocaleDateString('en-GB')
+                    const weekDay = dateObject.toLocaleDateString('en-US', { weekday: 'long' })
+
                     return <div key={index}>
                         <p>Barcode : {obj.barcode}</p>
-                        <p>Scanned On : {obj.timeStamp}</p>
+                        <p>Scanned On : {formattedDate},{formattedTime},{weekDay}</p>
                     </div>
                 })}</div>
             }
