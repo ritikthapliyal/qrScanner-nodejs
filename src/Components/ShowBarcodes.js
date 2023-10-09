5b5a3271a03760cba9ebab8506cb2a5e274c069f
ritikthapliyal <ritikthapliyal123@gmail.com>
2023-10-09 18:57:53 +0530

    testing ---100

--- src/Components/ShowBarcodes.js
+++ src/Components/ShowBarcodes.js
@@ -1,45 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useGetBarcodeQuery } from '../store/apis/dashboardApis';
 import Loading from './Loading';
-
 const divCss = {
   display: 'flex',
-  flexDirection: 'row',
+  flexDirection: 'column',
   alignItems: 'center',
-  justifyContent: 'space-between',
+  justifyContent: 'center',
   height: 'fit-content',
 };
-
 function ShowBarcodes() {
   const { data, isLoading } = useGetBarcodeQuery();
   const [currentPage, setCurrentPage] = useState(1);
   const resultsPerPage = 6;
-
+  const [sortedData, setSortedData] = useState([]);
   const startIndex = (currentPage - 1) * resultsPerPage;
   const endIndex = startIndex + resultsPerPage;
-
   const handleNextPage = () => {
     setCurrentPage(currentPage + 1);
   };
-
   const handlePrevPage = () => {
-    setCurrentPage(currentPage - 1);
+    setCurrentPage(currentPage - 1)
   };
-
+  const pageCount = Math.ceil(sortedData.length / resultsPerPage);
+  useEffect(() => {
+    if (data && data.message) {
+      // Sort the data by timestamp in descending order
+      const sorted = data.message.slice().sort((a, b) => {
+        return new Date(b.timeStamp) - new Date(a.timeStamp);
+      });
+      setSortedData(sorted);
+    }
+  }, [data]);
   return (
     <div className='show-barcodes'>
       {isLoading && <Loading style={{ fontSize: '18px' }} />}
-      {data && data.message && (
+      {sortedData.length > 0 && (
         <div>
           <div style={divCss}>
             <h3>Barcode History : </h3>
             <span style={{ fontSize: '12px' }}>
-              Total : {data && data.message ? data.message.length : '0'}
+              Total : {sortedData.length}
             </span>
           </div>
-
           <div style={{ height: 'fit-content' }}>
-            {data.message
+            {sortedData
               .slice(startIndex, endIndex)
               .map((obj, index) => {
                 const dateObject = new Date(obj.timeStamp);
@@ -48,7 +52,6 @@ function ShowBarcodes() {
                 const weekDay = dateObject.toLocaleDateString('en-US', {
                   weekday: 'long',
                 });
-
                 return (
                   <div key={index}>
                     <p>
@@ -62,15 +65,29 @@ function ShowBarcodes() {
                 );
               })}
           </div>
-
           <div style={divCss}>
-            <button onClick={handlePrevPage} disabled={currentPage === 1}>
+            <button
+              onClick={handlePrevPage}
+              disabled={currentPage === 1}
+              className="pagination-button"
+            >
               Prev
             </button>
-            <span>{currentPage}</span>
+            {Array.from({ length: pageCount }, (_, index) => (
+              <button
+                key={index}
+                onClick={() => setCurrentPage(index + 1)}
+                className={`pagination-button ${
+                  currentPage === index + 1 ? 'active-page' : ''
+                }`}
+              >
+                {index + 1}
+              </button>
+            ))}
             <button
               onClick={handleNextPage}
-              disabled={!data.message || endIndex >= data.message.length}
+              disabled={currentPage === pageCount}
+              className="pagination-button"
             >
               Next
             </button>
@@ -80,5 +97,4 @@ function ShowBarcodes() {
     </div>
   );
 }
-
-export default ShowBarcodes;
+export default ShowBarcodes
